50145cc3646fa3b048936eb0a1dc4cbeba961903
Yiming <103970218+Yiming0318@users.noreply.github.com>
2022-10-12 19:16:47 -0700

    Add files via upload

--- stock_download.py
+++ stock_download.py
@@ -9,14 +9,31 @@ ticker_names = ["AAPL", "MSFT", "GOOG", "AMZN", "TSLA",
                 "ORCL", "CSCO", "ACN", "WFC", "VZ", "COP", "NEE", "CRM",
                 "BMY", "TXN", "SCHW", "UPS", "NKE", "LIN", "QCOM", "MS",
                 "ADBE", "PM"]
+tn_sample = ["AAPL", "MSFT"]
 data_num = []
 data_sum = 0
-for ticker in ticker_names:
-    data = yf.download(ticker,"2012-01-01","2022-10-10")
-    data_num.append((ticker, len(data)))
-    data_sum += len(data)
-    path = "/Users/yiming/Desktop/easy-invest/stocks_data/"
-    data.to_csv(path + ticker + ".csv")
+total_data_list = []
+# for index, ticker in enumerate(ticker_names):
+#     data = yf.download(ticker,"2012-01-01","2022-10-10")
+#     data["StockID"] = index + 1
+#     data["TickerName"] = ticker
+    # data_num.append((ticker, len(data)))
+    # data_sum += len(data)
+    # path = "/Users/yiming/Desktop/easy-invest/stocks_data/"
+    # data.to_csv(path + ticker + ".csv")
 
-print(data_num)
-print(data_sum)
+# print(data_num)
+# print(data_sum)
+
+total_data_list = []
+for index, ticker in enumerate(tn_sample):
+    data = yf.download(ticker,"2012-01-01","2022-10-10")
+    data["StockID"] = data["Date"] + data["TickerName"]
+    data["TickerName"] = ticker
+    total_data_list.append(data)
+
+res = pd.concat(total_data_list)
+# path = "/Users/yiming/Desktop/easy-invest/stocks_data/"
+# res.to_csv(path + "res.csv")
+print(res.head())
+print(len(res))
